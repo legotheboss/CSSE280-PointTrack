396d7395425a9c08ec8ba6556d61d3d9375cfbb6
Amruth Pabba <amruthpabba@gmail.com>
2021-02-19 20:49:59 -0500

    [AP] major functional

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,21 @@
 var rhit = rhit || {};
 
 rhit.FB_COLLECTION_RewardAccount = "pointtrack";
+rhit.FB_COLLECTION_PointHistory = "point_history";
 rhit.FB_KEY_LAST_UPDATED = "reward_history";
 rhit.FB_KEY_CUR_BALANCE = "current_balance";
 rhit.FB_KEY_CARD = "accountType";
 rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
-rhit.FB_KEY_BALANCE_HISTORY = "point_history";
+rhit.FB_KEY_POINTHISTORY_DATE = "timestamp";
+rhit.FB_KEY_POINTHISTORY_BALANCE = "balance"
 rhit.KEY_UID = "uid";
 rhit.fbRewardAccountsManager = null;
 rhit.fbSingleAccountManager = null;
 rhit.fbAuthManager = null;
+rhit.fbCashValue = null;
+rhit.chart = null;
 
-const accountEnums = Object.freeze({"amexMR":"American Express Membership Rewards", "citiTYP":"Citi ThankYou Rewards", "chaseUMR":"Chase Ultimate Rewards", "boaPR":"Bank of America Premier Rewards", "discoverCR":"Discover Card Rewards"});
+const accountEnums = Object.freeze({"amexMR":"American Express Rewards", "citiTYP":"Citi ThankYou", "chaseUMR":"Chase Ultimate Rewards", "boaPR":"Bank of America Premier Rewards", "discoverCR":"Discover Card Rewards"});
 
 // From: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
 function htmlToElement(html) {
@@ -51,22 +55,24 @@ rhit.ListPageController = class {
 		});
 		$("#addQuoteDialog").on("shown.bs.modal", (event) => {
 			// Post animation
-			//document.querySelector("#inputQuote").focus();
+			document.querySelector("#inputMovie").focus();
 		});
 
 		// Start listening!
 		rhit.fbRewardAccountsManager.beginListening(this.updateList.bind(this));
-
 	}
 
-
 	updateList() {
-
-		document.querySelector("#nameBadge").innerHTML = `Welcome, ${rhit.fbAuthManager._user.displayName}!`;
+		if(rhit.fbAuthManager._user.displayName){
+			var title_text = `Welcome, ${rhit.fbAuthManager._user.displayName}!`;
+		} else {
+			var title_text = "Welcome!"
+		}
+		document.querySelector("#nameBadge").innerHTML = title_text;
+		
 
 		console.log("I need to update the list on the page!");
 		console.log(`Num quotes = ${rhit.fbRewardAccountsManager.length}`);
-		console.log("Example quote = ", rhit.fbRewardAccountsManager.getRewardAccountAtIndex(0));
 
 		// Make a new quoteListContainer
 		const newList = htmlToElement('<div id="quoteListContainer"></div>');
@@ -94,9 +100,10 @@ rhit.ListPageController = class {
 	_createCard(RewardAccount) {
 		return htmlToElement(`<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">${eval("accountEnums."+RewardAccount.accountType)}</h5>
-			<h6 class="card-subtitle mb-2 text-muted">${RewardAccount.movie}</h6>
-			<h6 class="card-subtitle mb-2 text-muted">Last Updated: ${RewardAccount.quote}</h6>
+			<h5 class="card-title" style="font-size: large;">${eval("accountEnums."+RewardAccount.accountType)}</h5>
+			<h6 class="card-subtitle mb-2 text-muted" style="font-size: medium;">${RewardAccount.current_bal} points</h6>
+			<h6 class="card-subtitle mb-2 text-muted" style="font-size: medium;">Last Updated: ${RewardAccount.last_updated.toDate().toLocaleDateString(
+				'en-us')}</h6>
 		</div>
 	</div>`);
 	}
@@ -104,10 +111,10 @@ rhit.ListPageController = class {
 }
 
 rhit.RewardAccount = class {
-	constructor(id, quote, movie, accountType) {
+	constructor(id, last_updated, current_bal, accountType) {
 		this.id = id;
-		this.quote = quote;
-		this.movie = movie;
+		this.last_updated = last_updated;
+		this.current_bal = current_bal;
 		this.accountType = accountType;
 	}
 }
@@ -120,11 +127,11 @@ rhit.FbRewardAccountsManager = class {
 		this._unsubscribe = null;
 	}
 
-	add(quote, movie, cardAccount) {
+	add(last_updated, cur_balance, cardAccount) {
 		// Add a new document with a generated id.
 		this._ref.add({
-				[rhit.FB_KEY_LAST_UPDATED]: quote,
-				[rhit.FB_KEY_CUR_BALANCE]: movie,
+				[rhit.FB_KEY_LAST_UPDATED]: last_updated,
+				[rhit.FB_KEY_CUR_BALANCE]: cur_balance,
 				[rhit.FB_KEY_CARD]: cardAccount,
 				[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
 				[rhit.KEY_UID]: rhit.fbAuthManager.uid,
@@ -147,9 +154,6 @@ rhit.FbRewardAccountsManager = class {
 		this._unsubscribe = query.onSnapshot((querySnapshot) => {
 			console.log("RewardAccount update!");
 			this._documentSnapshots = querySnapshot.docs;
-			// querySnapshot.forEach((doc) => {
-			// 	console.log(doc.data());
-			// });
 			changeListener();
 		});
 	}
@@ -158,8 +162,6 @@ rhit.FbRewardAccountsManager = class {
 		this._unsubscribe();
 	}
 
-	// update(id, quote, movie) {}
-	// delete(id) {}
 	get length() {
 		return this._documentSnapshots.length;
 	}
@@ -181,19 +183,23 @@ rhit.DetailPageController = class {
 			rhit.fbAuthManager.signOut();
 		});
 		document.querySelector("#submitEditQuote").addEventListener("click", (event) => {
-			//const quote = document.querySelector("#inputQuote").value;
-			const movie = document.querySelector("#inputMovie").value;
-			rhit.fbSingleAccountManager.update(quote, movie);
+			const balance = document.querySelector("#inputBalance").value;
+			const date = document.querySelector('#start').valueAsDate;
+			rhit.fbSingleAccountManager.addDatapoint(date, balance);
 		});
 
-		$("#editQuoteDialog").on("show.bs.modal", (event) => {
-			// Pre animation
-			//document.querySelector("#inputQuote").value = rhit.fbSingleAccountManager.quote;
-			document.querySelector("#inputMovie").value = rhit.fbSingleAccountManager.movie;
+		document.querySelector("#menuShowMyQuotes").addEventListener("click", (event) => {
+			window.location.href = `/home.html`;
 		});
+
+		document.querySelector("#menuSignOut").addEventListener("click", (event) => {
+			//console.log("Sign out");
+			rhit.fbAuthManager.signOut();
+		});
+
 		$("#editQuoteDialog").on("shown.bs.modal", (event) => {
 			// Post animation
-			document.querySelector("#inputQuote").focus();
+			document.querySelector("#inputBalance").focus();
 		});
 
 		document.querySelector("#submitDeleteQuote").addEventListener("click", (event) => {
@@ -205,12 +211,71 @@ rhit.DetailPageController = class {
 			});
 		});
 
+		var ctx = document.getElementById('myChart').getContext('2d');
+		Chart.defaults.global.defaultFontColor='white';
+		rhit.chart = new Chart(ctx, {
+			// The type of chart we want to create
+			type: 'line',
+		
+			// The data for our dataset
+			data: {
+				//labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+				datasets: [{
+					backgroundColor: 'rgb(220, 220, 220)',
+					borderColor: 'rgb(255, 255, 255)',
+					data: [{
+						x: new Date().setDate(15),
+						y: 1
+					}, {
+						t: new Date().setDate(16),
+						y: 5
+					}, {
+						t: new Date().setDate(17),
+						y: 10
+					}],
+				}]
+			},
+
+			options: {
+				legend: {
+					display: false
+				},
+				layout: {
+					padding: {
+						left: 0,
+						right: 0,
+						top: 5,
+						bottom: 5
+					}
+				},
+				scales: {
+					xAxes: [{
+						gridLines: {
+							display:false
+						},
+						type: 'time',
+						time: {
+							unit: 'day'
+						}
+					}],
+					yAxes: [{
+						gridLines: {
+							display:false
+						},
+						display: false   
+					}]
+				}
+			}
+		
+			// Configuration options go here
+		});
 		rhit.fbSingleAccountManager.beginListening(this.updateView.bind(this));
+		rhit.fbCashValue.beginListening(this.updateView.bind(this));
 	}
 	updateView() {
 		let account = rhit.fbSingleAccountManager.cardAccount;
 		document.querySelector("#cardQuote").innerHTML =  eval("accountEnums."+account);
-		document.querySelector("#cardMovie").innerHTML = rhit.fbSingleAccountManager.movie+" Points";
+		document.querySelector("#cardMovie").innerHTML = rhit.fbSingleAccountManager.cur_balance+" Points";
 		if (rhit.fbSingleAccountManager.uid == rhit.fbAuthManager.uid) {
 			document.querySelector("#menuEdit").style.display = "flex";
 			document.querySelector("#menuDelete").style.display = "flex";
@@ -223,6 +288,9 @@ rhit.FbSingleAccountManager = class {
 		this._documentSnapshot = {};
 		this._unsubscribe = null;
 		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_RewardAccount).doc(RewardAccountId);
+		this._point_balance_ref = this._ref.collection(rhit.FB_COLLECTION_PointHistory);
+		this.point_history = [];
+		this.redemption_methods = [];
 	}
 
 	beginListening(changeListener) {
@@ -230,6 +298,11 @@ rhit.FbSingleAccountManager = class {
 			if (doc.exists) {
 				console.log("Document data:", doc.data());
 				this._documentSnapshot = doc;
+				firebase.firestore().collection("redemptions").doc(this.cardAccount).get().then(
+					(docu) => {
+						this.redemption_methods = docu.data();
+					}
+				)
 				changeListener();
 			} else {
 				// doc.data() will be undefined in this case
@@ -243,10 +316,10 @@ rhit.FbSingleAccountManager = class {
 		this._unsubscribe();
 	}
 
-	update(quote, movie, cardAccount) {
+	update(last_updated, cur_balance, cardAccount) {
 		this._ref.update({
-				[rhit.FB_KEY_LAST_UPDATED]: quote,
-				[rhit.FB_KEY_CUR_BALANCE]: movie,
+				[rhit.FB_KEY_LAST_UPDATED]: last_updated,
+				[rhit.FB_KEY_CUR_BALANCE]: cur_balance,
 				[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
 				[rhit.FB_KEY_CARD]: cardAccount
 			})
@@ -259,15 +332,46 @@ rhit.FbSingleAccountManager = class {
 			});
 	}
 
+	addDatapoint(date, balance) {
+		// Add a new document with a generated id.
+		this._point_balance_ref.add({
+				[rhit.FB_KEY_POINTHISTORY_BALANCE]: parseInt(balance),
+				[rhit.FB_KEY_POINTHISTORY_DATE]: date,
+			})
+			.then(function (docRef) {
+				console.log("Subcollection added with ID: ", docRef.id);
+			})
+			.catch(function (error) {
+				console.error("Error adding document: ", error);
+			});
+		
+		this._ref.update({
+			[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
+		})
+		.then(() => {
+			console.log("Document successfully updated!");
+		})
+		.catch(function (error) {
+			// The document probably doesn't exist.
+			console.error("Error updating document: ", error);
+		});
+	}
+
+	async updatePointHistory() {
+		const snapshot = await this._point_balance_ref.get();
+		this.point_history = snapshot.docs.map(doc => doc.data());
+		return this.point_history[0];
+	}
+
 	delete() {
 		return this._ref.delete();
 	}
 
-	get quote() {
+	get last_updated() {
 		return this._documentSnapshot.get(rhit.FB_KEY_LAST_UPDATED);
 	}
 
-	get movie() {
+	get cur_balance() {
 		return this._documentSnapshot.get(rhit.FB_KEY_CUR_BALANCE);
 	}
 
@@ -278,9 +382,38 @@ rhit.FbSingleAccountManager = class {
 	get cardAccount() {
 		return this._documentSnapshot.get(rhit.FB_KEY_CARD);
 	}
+}
 
-	
+rhit.FbCashValue = class {
+	constructor() {
+		this._documentSnapshot = {};
+		this._unsubscribe = null;
+		this._ref = firebase.firestore().collection("cash_value").doc("current");
+	}
 
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+				console.log("Document data:", doc.data());
+				this._documentSnapshot = doc;
+				const cash_equivalent = rhit.fbCashValue.getValue(rhit.fbSingleAccountManager.cardAccount)* rhit.fbSingleAccountManager.cur_balance;
+				document.querySelector("#cashValue").innerHTML = "$" + cash_equivalent.toFixed(2);
+				changeListener();
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+				//window.location.href = "/";
+			}
+		});
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	getValue(accountType) {
+		return this._documentSnapshot.get("values")[accountType];
+	}
 }
 
 rhit.FbAuthManager = class {
@@ -366,6 +499,7 @@ rhit.initializePage = function () {
 			window.location.href = "/";
 		}
 		rhit.fbSingleAccountManager = new rhit.FbSingleAccountManager(RewardAccountId);
+		rhit.fbCashValue = new rhit.FbCashValue();
 		new rhit.DetailPageController();
 	}
 
@@ -386,7 +520,6 @@ rhit.main = function () {
 		rhit.checkForRedirects();
 		rhit.initializePage();
 	});
-
 	rhit.startFireBaseUI();
 };
 
@@ -397,8 +530,10 @@ rhit.startFireBaseUI = function() {
 			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 		],
 	};
-    const ui = new firebaseui.auth.AuthUI(firebase.auth());
-	ui.start('#loginButtons', uiConfig);
+    if(document.querySelector("#loginPage")) {
+		const ui = new firebaseui.auth.AuthUI(firebase.auth());
+		ui.start('#loginButtons', uiConfig);
+	}
 };
 
 
